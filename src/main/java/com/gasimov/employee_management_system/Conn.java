16878700a0739c1cf54e64d3c60172d68f6af947
eyyubqasimov <eyyub.qasimov98@gmail.com>
2023-06-09 12:02:41 +0400

    first commit

---
+++ src/main/java/com/gasimov/employee_management_system/Conn.java
@@ -0,0 +1,20 @@
+package com.gasimov.employee_management_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem", "bestuser", "bestuser");
+            s=c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/com/gasimov/employee_management_system/DatabaseUtils.java
@@ -0,0 +1,32 @@
+package com.gasimov.employee_management_system;
+
+import javax.swing.table.DefaultTableModel;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class DatabaseUtils {
+
+    public static DefaultTableModel resultSetToTableModel(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        Vector<String> columnNames = new Vector<>();
+        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+            columnNames.add(metaData.getColumnLabel(columnIndex));
+        }
+
+
+        Vector<Vector<Object>> data = new Vector<>();
+        while (rs.next()) {
+            Vector<Object> row = new Vector<>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                row.add(rs.getObject(columnIndex));
+            }
+            data.add(row);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+    }
+}
